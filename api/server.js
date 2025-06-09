2e25337e3076f6726735c77dff46d50fd7adb99f
SylvaDev <irvingcsylva@gmail.com>
2025-06-09 12:51:25 -0700

    added logic for updating dogs in database and fixed helper function for updating dog id

--- api/server.js
+++ api/server.js
@@ -43,14 +43,37 @@ server.get('/api/dogs/:id', async (req, res) => {
 server.post('/api/dogs', async (req, res) => {
     try{
         const { name, weight } = req.body;
-        const newDog = await Dog.create({ name, weight });
-        res.status(201).json(newDog);
+        if (!name || !weight) {
+            res.status(422).json({message: 'Name and weight are required'})
+        } else {
+            const newDog = await Dog.create({ name, weight });
+            res.status(201).json({message: 'Dog created', data: newDog});
+        }
     } catch(err) {
         res.status(500).json({message: `${err.message} - Error creating dog`})
     }
 });
 
 // [PUT]    /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
+server.put('/api/dogs/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        const { name, weight } = req.body;
+        if (!name || !weight) {
+            res.status(422).json({message: 'Name and weight are required'})
+        } else {
+            const updatedDog = await Dog.update(id, { name, weight });
+            if(!updatedDog) {
+                res.status(404).json({message: `No Dog with ID: ${id}`})
+            } else {
+                res.status(200).json({message: 'Dog updated', data: updatedDog});
+            } 
+        }
+    } catch(err) {
+        res.status(500).json({message: `${err.message} - Error updating dog`})
+    }
+});
+
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 
 // EXPOSING THE SERVER TO OTHER MODULES
