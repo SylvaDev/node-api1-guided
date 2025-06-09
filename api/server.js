8655ac008a958b4a34d9d4272db601082378fda7
SylvaDev <irvingcsylva@gmail.com>
2025-06-09 12:10:19 -0700

    created new server, added first endpoints for getting dogs

--- api/server.js
+++ api/server.js
@@ -1,17 +1,43 @@
 // IMPORTS AT THE TOP
 const express = require('express');
+const Dog = require('./dog-model');
+
 // INSTANCE OF EXPRESS APP
 const server = express();
+
 // GLOBAL MIDDLEWARE
 server.use(express.json());
+
 // ENDPOINTS
-server.get('/', (req, res) => {
 // [GET]    /             (Hello World endpoint)
+server.get('/hello-world', (req, res) => {
+    res.status(200).json({message: 'Hello, World'});
+});
+
 // [GET]    /api/dogs     (R of CRUD, fetch all dogs)
+server.get('/api/dogs', async (req, res) => {
+    try{
+        const dogs = await Dog.findAll();
+        res.status(200).json(dogs);
+    } catch(err) {
+        res.status(500).json({message: `${err.message} - Error fetching dogs`})
+    }
+});
+
 // [GET]    /api/dogs/:id (R of CRUD, fetch dog by :id)
+server.get('/api/dogs/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        const dog = await Dog.findById(id);
+       res.status(200).json(dog);
+    } catch(err) {
+        res.status(500).json({message: `${err.message} - Error fetching dog`})
+    }
+});
+
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
 // [PUT]    /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
-});
+
 // EXPOSING THE SERVER TO OTHER MODULES
-module.exports = server
+module.exports = server;
